205e4518a4018bc8e33009a5f299913f079ce56c
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2024-04-05 19:43:33 +0000

    how to declare variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+//const =syntax
+const acc_ID=13224
+let acc_email="abhi3003shek@semimetals.com"
+var acc_pass="@AbhishekJS01"
+acc_city="Rae Bareli" // not recommended to declare variable this type
+
+let acc_state; // what should be its value?  //undefined
+
+// we can declare variables in 2 ways let and var
+
+//prefer not to use var bcz of issue in block scope and functional scope
+
+
+console.log(acc_ID);
+//to avoid writing multiple console.log() statements we use console.table([])
+
+console.table([acc_ID,acc_email,acc_pass,acc_city,acc_state])
