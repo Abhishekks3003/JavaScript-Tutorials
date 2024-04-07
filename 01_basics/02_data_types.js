ba374611941bc22ff3628d5396889e13ee828931
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2024-04-07 18:43:58 +0000

    Data_Types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,22 @@
+"use strict"; //all code will be treated as newer version of JS
+//alert(3+3) //error bcz here everything is managed by node js not browser
+
+let name="Abhishek"
+let age=18
+let is_male=true;
+let state;
+
+
+// number ->
+// bigint
+// string ->
+// boolean -> T/F
+// null -> standalone datatype
+// undefined -> 
+// symbol => unique | used in React
+
+
+// object
+
+console.log(typeof null);        // object 
+console.log(typeof undefined);  // undefined
